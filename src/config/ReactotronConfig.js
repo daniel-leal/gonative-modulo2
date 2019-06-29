abf7b9cb071fec924513c32d22a712a3fc532919
daniel-leal <danielleal94@gmail.com>
2019-06-29 13:06:17 -0300

    init repo

---
+++ src/config/ReactotronConfig.js
@@ -0,0 +1,11 @@
+import Reactotron from 'reactotron-react-native';
+
+if (__DEV__) {
+  const tron = Reactotron.configure({ host: '10.0.3.2' }) // controls connection & communication settings
+    .useReactNative() // add all built-in react native plugins
+    .connect(); // let's connect!
+
+  console.tron = tron;
+
+  tron.clear();
+}
